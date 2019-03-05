283bbaf097e49a1f948b43fcf15da63e3582b9eb
mingxuts <ming.xie@uts.edu.au>
2019-03-05 15:27:51 +1100

    haha

--- src/main/java/org/cai/controller/GreetingController.java
+++ src/main/java/org/cai/controller/GreetingController.java
@@ -44,7 +44,7 @@ public class GreetingController {
     	String script = fileStorageService.saveText(taskdir, "script.txt", shscr.toString(env, dev, cmd, workdir));
     	
     	File scriptFile = new File(script);
-    	String ssh_cmd = "ssh delta <" + script;
+    	String ssh_cmd = "ssh gamma date";
     	//ssh_cmd += " |& tee " + scriptFile.getParent() + "/cmd.txt";
     	cmdService.executeCommand(ssh_cmd);
     }
